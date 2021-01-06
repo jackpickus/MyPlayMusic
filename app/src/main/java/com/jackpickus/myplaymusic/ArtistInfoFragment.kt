6922050276c0e6dc28e1d79421cf7c3f097e3e7d
jackpickus <jpick329@gmail.com>
2021-01-05 22:34:04 -0600

    Created view for artist fragment

--- app/src/main/java/com/jackpickus/myplaymusic/ArtistInfoFragment.kt
+++ app/src/main/java/com/jackpickus/myplaymusic/ArtistInfoFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.jackpickus.myplaymusic.models.Music
 
 const val ARG_ARTIST_ID:String = "music_artist"
 
 class ArtistInfoFragment : Fragment() {
+    private lateinit var mArtistTextView: TextView
+    private lateinit var mMusicArtist: String
 
     companion object {
         fun newInstance(musicArtist: String) : ArtistInfoFragment {
@@ -25,11 +28,11 @@ class ArtistInfoFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val musicArtist = activity?.intent?.getSerializableExtra(ARG_ARTIST_ID) as String
+        mMusicArtist = activity?.intent?.getSerializableExtra(ARG_ARTIST_ID) as String
         val songsByArtist = ArrayList<Music>()
 
         for (song in MusicListFragment.newMusics) {
-            if (song.artist == musicArtist) {
+            if (song.artist == mMusicArtist) {
                 songsByArtist.add(song)
             }
         }
@@ -37,7 +40,11 @@ class ArtistInfoFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val view = inflater.inflate(R.layout.fragment_about_artist, container, false)
+        mArtistTextView = view.findViewById(R.id.artist_title_text_view)
+        mArtistTextView.text = mMusicArtist
+
+        return view
     }
 
 }
