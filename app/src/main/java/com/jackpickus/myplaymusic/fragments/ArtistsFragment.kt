14537e1bf65d7c6d9f44987de1d43077d7e0a3c4
jackpickus <jpick329@gmail.com>
2021-01-31 14:32:57 -0600

    changed layout to reuse fragment_music_list layout

--- app/src/main/java/com/jackpickus/myplaymusic/fragments/ArtistsFragment.kt
+++ app/src/main/java/com/jackpickus/myplaymusic/fragments/ArtistsFragment.kt
@@ -10,7 +10,7 @@ import com.jackpickus.myplaymusic.R
 class ArtistsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_favorites, container, false)
+        val view = inflater.inflate(R.layout.fragment_music_list, container, false)
         return view
     }
 }
--- app/src/main/java/com/jackpickus/myplaymusic/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/jackpickus/myplaymusic/fragments/FavoritesFragment.kt
@@ -9,7 +9,7 @@ import com.jackpickus.myplaymusic.R
 
 class FavoritesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_favorites, container, false)
+        val view = inflater.inflate(R.layout.fragment_music_list, container, false)
         return view
     }
 }
